f9ffa24bd4d6bb212a14bd9c1a0c18e3e5fd6ae5
septe habudin <septe.vhirgo@gmail.com>
2023-02-01 16:41:11 +0700

    initial project

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Reminders App Clone 
+
+
+Reminders App Clone with SwiftUI & Core Data With [Mohammad Azam](https://www.udemy.com/course/building-a-reminders-app-clone-with-swiftui-core-data/)
---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by septe habudin on 01/02/23.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
